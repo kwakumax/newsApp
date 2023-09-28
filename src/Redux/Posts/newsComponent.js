0840f9c4e235fbd16326671548efbbd0ec7d94f2
kwakumax <andarmaas3@gmail.com>
2023-09-28 08:33:20 +0000

    version 1

---
+++ src/Redux/Posts/newsComponent.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchTopHeadlines,
+  fetchEverything,
+  selectTopHeadlinesData,
+  selectEverythingData,
+} from "./newsSlice"; // Importing thunks and selectors
+
+function NewsComponent() {
+  const dispatch = useDispatch();
+  const topHeadlinesData = useSelector(selectTopHeadlinesData);
+  const everythingData = useSelector(selectEverythingData);
+
+  useEffect(() => {
+    // Fetch data when the component mounts
+    dispatch(fetchTopHeadlines());
+    dispatch(fetchEverything());
+  }, []);
+
+  useEffect(() => {
+    // Fetch data when the component dispatches an action
+    dispatch(fetchTopHeadlines());
+    dispatch(fetchEverything());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h2>Top Headlines</h2>
+      {topHeadlinesData.status === "loading" && <p>Loading top headlines...</p>}
+      {topHeadlinesData.status === "failed" && (
+        <p>Error loading top headlines.</p>
+      )}
+      {topHeadlinesData.status === "succeeded" && (
+        <ul>
+          {topHeadlinesData.data.map((article, index) => (
+            <li key={index}>
+              <h3>{article.title}</h3>
+              <p>{article.description}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      <h2>Everything</h2>
+      {everythingData.status === "loading" && <p>Loading everything...</p>}
+      {everythingData.status === "failed" && <p>Error loading everything.</p>}
+      {everythingData.status === "succeeded" && (
+        <ul>
+          {everythingData.data.map((article, index) => (
+            <li key={index}>
+              <h3>{article.title}</h3>
+              <p>{article.description}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default NewsComponent;
+
---
+++ src/Redux/Posts/newsSlice.js
@@ -0,0 +1,80 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const apiKey = "8b9fe0757d70451eb95b84b06ca317bd"; //  API key gotten from newsapi.org
+
+// An Article type based on the properties returned from the API
+const Article = {
+  title: "",
+  description: "",
+};
+
+export const fetchTopHeadlines = createAsyncThunk(
+  "news/fetchTopHeadlines",
+  () => {
+    return axios
+      .get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
+      .then((response) => response.data.articles);
+  }
+);
+
+export const fetchEverything = createAsyncThunk("news/fetchEverything", () => {
+  return axios
+    .get(`https://newsapi.org/v2/everything?q=technology&apiKey=${apiKey}`)
+    .then((response) => response.data.articles);
+});
+
+const initialState = {
+  topHeadlines: {
+    data: [],
+    status: "idle",
+    error: null,
+  },
+  everything: {
+    data: [],
+    status: "idle",
+    error: null,
+  },
+};
+
+const newsSlice = createSlice({
+  name: "news",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTopHeadlines.pending, (state) => {
+        state.topHeadlines.status = "loading";
+      })
+      .addCase(fetchTopHeadlines.fulfilled, (state, action) => {
+        state.topHeadlines.status = "succeeded";
+        state.topHeadlines.data = action.payload;
+      })
+      .addCase(fetchTopHeadlines.rejected, (state, action) => {
+        state.topHeadlines.status = "failed";
+        // state.topHeadlines.error = action.error.message;
+      })
+      .addCase(fetchEverything.pending, (state) => {
+        state.everything.status = "loading";
+      })
+      .addCase(fetchEverything.fulfilled, (state, action) => {
+        state.everything.status = "succeeded";
+        state.everything.data = action.payload;
+      })
+      .addCase(fetchEverything.rejected, (state, action) => {
+        state.everything.status = "failed";
+        // state.everything.error = action.error.message;
+      });
+  },
+});
+
+export const selectTopHeadlinesData = (state) =>
+  state.news.topHeadlines.data;
+export const selectEverythingData = (state) =>
+  state.news.everything.data;
+
+export default newsSlice.reducer;
+
+
+
+
